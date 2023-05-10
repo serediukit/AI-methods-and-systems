563a7d9ef548b8101fc49b99f5ce9ed41d51c43f
Valentyn Serediuk <serediukit@gmail.com>
2023-05-10 20:53:01 +0300

    add files

---
+++ Wav_NN_Learn.py
@@ -0,0 +1,240 @@
+import numpy as np
+import tensorflow as tf
+import random as rn
+import os
+os.environ['PYTHONHASHSEED'] = '0'
+
+np.random.seed(42)
+rn.seed(12345)
+from keras.layers import Convolution2D, BatchNormalization, MaxPooling2D, Dense, Input, Dropout, Flatten
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.models import Model
+from keras.callbacks import TensorBoard
+from keras.models import Sequential
+from keras.optimizers import Adam
+import time
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from keras.callbacks import CSVLogger
+from keras.callbacks import ModelCheckpoint
+from keras.models import load_model
+from scipy import signal
+from scipy.io import wavfile
+import random
+
+def Load_Wav_(WorkDir):
+    Input_Files = []
+    Source_Samples= []
+    for d, dirs, files in os.walk(WorkDir):
+        for file in files:
+            Input_Files.append(file)
+    GFile  = []
+    for file in Input_Files:
+            if file.endswith(".wav"):
+                sample_rate, samples = wavfile.read(str(WorkDir)+ file)
+                if sample_rate != 8000:
+                    continue
+                if max(abs(samples)) < 410:
+                    continue
+                if len(samples) < int(0.1 * sample_rate):
+                    continue
+                GFile.append(file)
+#                 samples = Convert_To_06(samples)
+                Source_Samples.append(samples)
+
+    return  Source_Samples, GFile
+
+
+
+def Add_Zero(specgram, TargetColumnNumber, StartSignalPosition):
+
+    if len(specgram[0]) >= TargetColumnNumber:
+         return specgram
+
+    full_array = np.zeros((len(specgram), TargetColumnNumber))
+    full_array[:, :len(specgram[0])-TargetColumnNumber] = specgram
+    if StartSignalPosition == 0:
+        full_array[:, :len(specgram[0]) - TargetColumnNumber] = specgram
+    elif StartSignalPosition == TargetColumnNumber - len(specgram[0]):
+        full_array[:, StartSignalPosition:] = specgram
+    else:
+        full_array[:, StartSignalPosition:StartSignalPosition+len(specgram[0]) - TargetColumnNumber] = specgram
+
+    return full_array
+def log_specgram(audio, window_size, sample_rate=8000,
+                 eps=1e-10, windoe_fuction='hann'):
+    nperseg = int(round(window_size * sample_rate / 1000))
+    noverlap = int(round(window_size / 2 * sample_rate / 1000))
+    freqs, times, spec = signal.spectrogram(audio,
+                                            fs=sample_rate,
+                                            window=windoe_fuction,
+                                            nperseg=nperseg,
+                                            noverlap=noverlap,
+                                            detrend=False)
+    return freqs, times, np.log(spec.astype(np.float32) + eps)
+def Convert_Wav_To_specgram(SamplesList, Input_Files, window_size, windoe_fuction):
+    samples = np.zeros(int(0.6 * 8000))
+    _, _, specgram = log_specgram(audio=samples, window_size=window_size, windoe_fuction=windoe_fuction)
+    TargetColumnNumber = len(specgram[0])
+    TargetRow = len(specgram)
+    x = []
+    y = []
+    for i in range(len(SamplesList)):
+        _, _, specgram = log_specgram(audio=SamplesList[i], window_size=window_size, windoe_fuction=windoe_fuction)
+        specgram = Add_Zero(specgram, TargetColumnNumber, 0)
+        x.append(specgram)
+        file = Input_Files[i]
+        if 'cl_1' in file:
+            y.append([0, 0, 1])
+        elif 'cl_2' in file:
+            y.append([1, 0, 0])
+        else:
+            y.append([0, 1, 0])
+
+
+    x = np.array(x)
+    x = x.reshape(tuple(list(x.shape) + [1]))
+    y = np.array(y)
+    return x, y
+def RandomozeArrays(SourceArrayX, SourceArrayY):
+    TargetArrayX=[]
+    TargetArrayY=[]
+    while 0 < len(SourceArrayX):
+        Index = random.randint(0, len(SourceArrayX) - 1)
+        TargetArrayX.append(SourceArrayX[Index])
+        del SourceArrayX[Index]
+        TargetArrayY.append(SourceArrayY[Index])
+        del SourceArrayY[Index]
+
+    return TargetArrayX,TargetArrayY
+
+def Learn_NN_5L_(TrainDir,ValidDir, RezDir,NN_Name,Epochs=30, window_size=25, windoe_fuction='hann'):
+    Source_Samples, Input_Files= Load_Wav_(TrainDir)
+    print('end load train data', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+    Source_Samples, Input_Files = RandomozeArrays(Source_Samples, Input_Files)
+    print('end Randomize train data', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+
+
+    X_Train, Y_Train = Convert_Wav_To_specgram(SamplesList=Source_Samples, Input_Files=Input_Files,
+                                           window_size=window_size, windoe_fuction=windoe_fuction)
+    print('end convert train data', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+
+    Source_Samples, Input_Files = Load_Wav_(ValidDir)
+    print('end load valid data', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+
+    X_val1, y_val1 = Convert_Wav_To_specgram(SamplesList=Source_Samples, Input_Files=Input_Files,
+                                                     window_size=window_size, windoe_fuction=windoe_fuction)
+    print('end convert valid data', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+
+    input_shape = (X_Train.shape[1], X_Train.shape[2], 1)
+    model = Sequential()
+
+    model.add(BatchNormalization(input_shape = input_shape))
+    model.add(Convolution2D(48, (5, 5), strides = (3, 3), padding = 'same',input_shape = input_shape))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2,2), padding = 'same'))
+    model.add(BatchNormalization())
+    model.add(Convolution2D(40, (3, 3), strides = (2, 2), padding = 'same'))
+    model.add(Activation('relu'))
+    model.add(Flatten())
+#     model.add(Dense(45))
+#     model.add(Activation('relu'))
+#     model.add(Dense(30))
+#     model.add(Activation('relu'))
+    model.add(Dense(15))
+    model.add(Activation('relu'))
+    model.add(Dense(10))
+    model.add(Activation('relu'))
+    model.add(Dense(3))
+    model.add(Activation('softmax'))
+
+    model.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
+    csv_logger = CSVLogger(RezDir+NN_Name+'_training__log.csv', separator=',', append=False)
+
+
+    checkpoint = ModelCheckpoint(filepath=RezDir+NN_Name+'_Best.hdf5',
+                 monitor='val_accuracy',
+                 save_best_only=True,
+                 mode='max',
+                 verbose=1)
+    model.fit(X_Train, Y_Train,
+          batch_size = 64,
+          epochs = Epochs,shuffle=True,
+          validation_data=(X_val1, y_val1),
+          callbacks=[checkpoint, csv_logger])
+    model.save(filepath=RezDir+NN_Name+'_Final.hdf5')
+
+def TestNN_(NetName, SourceDir, TargetFile, window_size):
+    Input_Files = []
+    Source_Samples = []
+    for d, dirs, files in os.walk(SourceDir):
+        for file in files:
+            if file.endswith(".wav"):
+                sample_rate, samples = wavfile.read(SourceDir + file)
+                if sample_rate != 8000:
+                    continue
+                if max(abs(samples)) < 410:
+                    continue
+                if len(samples) < int(0.1 * sample_rate):
+                    continue
+                Input_Files.append(file)
+#                 samples = Convert_To_06(samples)
+                Source_Samples.append(samples)
+
+    x, y = Convert_Wav_To_specgram(SamplesList=Source_Samples, Input_Files=Input_Files,
+                                             window_size=window_size, windoe_fuction='hann')
+
+    new_model = load_model(NetName)
+    pred = new_model.predict(x)
+    f = open(TargetFile+'_FilesReport.csv', 'w', newline='\n')
+    f.write('NetName = %s, Files %s \n'%(NetName,SourceDir))
+    f.write('File Name,Marked As,Recognized As,Cl 1,Cl 2,Cl 3, \n')
+    CodeList = ['Cl 2', 'Cl 3', 'Cl 1']
+    SemplCount= [0,0,0]
+    StatRez = [[0,0,0],[0,0,0],[0,0,0]]
+    for i in range(len(pred)):
+        YY = list(y[i])
+        Rez = list(pred[i])
+        TrueCalss = YY.index(max(YY))
+        NNClass = Rez.index(max(Rez))
+        SemplCount[TrueCalss] +=1
+        StatRez[TrueCalss][NNClass] += 1
+        f.write( '%s,%s,%s, %f , %f, %f,\n' % (Input_Files[i], CodeList[TrueCalss], CodeList[NNClass], pred[i][2], pred[i][0], pred[i][1]))
+
+    f.close()
+    f = open(TargetFile+'_Report.csv', 'w', newline='\n')
+    f.write('NetName = %s, Files %s \n'%(NetName,SourceDir))
+    f.write('Var,Cl 1,Cl 2,Cl 3, \n')
+
+    f.write(  'Count,%s,%s, %s ,\n' % (SemplCount[2],SemplCount[0], SemplCount[1]))
+    f.write('Cl 1 As,%s,%s, %s ,\n' % (StatRez[2][2], StatRez[2][0], StatRez[2][1]))
+    f.write('Cl 2 As,%s,%s, %s ,\n' % (StatRez[0][2], StatRez[0][0], StatRez[0][1]))
+    f.write('Cl 3 As,%s,%s, %s ,\n' % (StatRez[1][2], StatRez[1][0], StatRez[1][1]))
+    trueclass =100.0* (StatRez[2][2] + StatRez[0][0] + StatRez[1][1])/ float( sum(SemplCount))
+
+    for i in range(len(SemplCount)):
+        for k in range(len(SemplCount)):
+            if SemplCount[i] > 0:
+                StatRez[i][k]=100.0*float(StatRez[i][k])/SemplCount[i]
+    f.write('Cl 1 As %%,%.3f%%,%.3f%%, %.3f%% ,\n' % (StatRez[2][2], StatRez[2][0], StatRez[2][1]))
+    f.write('Cl 2  As %%,%.3f%%,%.3f%%, %.3f%% ,\n' % (StatRez[0][2], StatRez[0][0], StatRez[0][1]))
+    f.write('Cl 3 As %%,%.3f%%,%.3f%%, %.3f%% ,\n' % (StatRez[1][2], StatRez[1][0], StatRez[1][1]))
+
+    f.write(',\n')
+
+    f.write('Total acc. ,%.3f%% ,\n' % (trueclass))
+
+    f.close()
+
+print('---start Learn---', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+Learn_NN_5L_(TrainDir=r'C:\Users\valik\Downloads\Lab\Train\\',
+             ValidDir=r'C:\Users\valik\Downloads\Lab\Valid\\',
+             RezDir=r'C:\Users\valik\Downloads\Lab\rez_dir\\',
+             NN_Name='NN_L5', Epochs=5, window_size=25, windoe_fuction='hann')
+
+print('---end  Learn---', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+
+TestNN_(NetName=r'C:\Users\valik\Downloads\Lab\rez_dir\NN_L5_Best.hdf5',
+            SourceDir=r'C:\Users\valik\Downloads\Lab\Test/',
+            TargetFile=r'C:\Users\valik\Downloads\Lab\rez_dir\NN_L5_rez',
+            window_size=25)
---
+++ Wav_data_prep.py
@@ -0,0 +1,204 @@
+import numpy as np
+import os
+os.environ['PYTHONHASHSEED'] = '0'
+np.random.seed(42)
+from scipy.io import wavfile
+from sklearn.preprocessing import LabelBinarizer
+labelbinarizer = LabelBinarizer()
+from scipy import signal
+from shutil import copyfile
+import random
+
+def Convert_To_06(samples):
+    sample_rate=8000
+    WindowSize = 40
+    LeftSeek = 300
+    RezFileLength = 0.6
+
+
+    if  len(samples) <= int(0.6*sample_rate):
+        return samples
+
+    Samples_SUM = np.arange(len(samples), dtype='float64')
+    ResultS = np.arange(int(RezFileLength * sample_rate), dtype=samples.dtype)
+    Samples_Float = np.arange(len(samples), dtype='float64')
+
+    # ------ARRAY -----AverageSum--------------------------
+    Average = 0.0
+    for i in range(len(samples)):
+        Average += samples[i]
+
+    Average = Average / len(samples)
+    for i in range(len(samples)):
+        Samples_Float[i] = abs(samples[i] - Average)
+
+    MaxSum = 0
+    TotalSum = 0
+    for i in range(len(Samples_SUM)):
+        Samples_SUM[i] = 0
+    i = WindowSize
+    while i < (len(samples) - WindowSize - 1):
+        CurentSum = 0
+        J = -WindowSize
+        while J < WindowSize:
+            CurentSum = CurentSum + Samples_Float[i + J]
+            J = J + 1
+        Samples_SUM[i] = CurentSum / (2 * WindowSize + 1)
+        if MaxSum < Samples_SUM[i]:
+            MaxSum = Samples_SUM[i]
+
+        i += 1
+    Thresholt = MaxSum / 6
+    for asum in Samples_SUM:
+        if Thresholt < asum:
+            TotalSum += asum
+    CenterIndex = 0
+    CurentSum = Samples_SUM[0]
+
+    while CurentSum < TotalSum / 2:
+        CenterIndex += 1
+        CurentSum += Samples_SUM[CenterIndex]
+
+    StartIndex = 0
+    BitRateCount = 0
+    for i in range(len(Samples_SUM)):
+        if Samples_SUM[i] > Thresholt:
+            BitRateCount += 1
+        else:
+            BitRateCount = 0
+        if BitRateCount > WindowSize * 4:
+            if i > WindowSize * 4 + LeftSeek:
+                StartIndex = int(i - (WindowSize * 4 + LeftSeek))
+            break
+
+
+    if CenterIndex < int(StartIndex + (sample_rate * RezFileLength / 2)) and (sample_rate * RezFileLength / 2) < CenterIndex:
+        StartIndex = CenterIndex - (sample_rate * RezFileLength / 2)
+    if StartIndex + sample_rate * RezFileLength > len(samples):
+        StartIndex = len(samples) - sample_rate * RezFileLength
+    StartIndex = int(StartIndex)
+    for i in range(int(RezFileLength * sample_rate)):
+        ResultS[i] = samples[i + StartIndex]
+    return  ResultS
+
+def ConvertTo_8K(SourceDir, TargetDirectory, Prefics, ClassType ):
+    #  -------Load Files----------------------------------------
+    Wav_Files = []
+    for d, dirs, files in os.walk(SourceDir):
+        for file in files:
+            if file.endswith(".wav"):
+                Wav_Files.append(file)
+    #  -------Convert to 8K and Raname and Save Files----------------------------------------
+    for wfile in Wav_Files:
+        curerentFile = SourceDir + "/" + wfile
+        sample_rate, samples = wavfile.read(curerentFile)
+
+        i = wfile.find(".wav")
+        FileName = wfile[:i]
+        FileName = FileName.replace("_nohash_", "")
+
+        if sample_rate == 16000:
+            lennewarray = int(len(samples) / 2)
+            f = signal.resample(samples, lennewarray)
+            Samples_1 = np.arange(lennewarray, dtype=samples.dtype)
+            for i in range(lennewarray):
+                Samples_1[i] = f[i]
+            curerentFile = TargetDirectory+ "\\"+ Prefics + FileName + "_0000_" + ClassType + ".wav"
+            Samples_final = Convert_To_06(Samples_1)
+            wavfile.write(curerentFile, 8000, Samples_final)
+
+        if sample_rate == 8000:
+            curerentFile = TargetDirectory + FileName + "+0000_" + ClassType + ".wav"
+            Samples_final = Convert_To_06(samples)
+            wavfile.write(curerentFile, sample_rate, Samples_final)
+
+
+def Divide_TrainTestValid(SourceDirectory, TrainDirectory, TestDirectory, ValidDirectory,TestPercent, ValidPercent):
+
+    Clas1_Files = []
+    Clas2_Files = []
+    Clas3_Files = []
+    Test_Files = []
+    Clas1_ID = "cl_1"
+    Clas2_ID = "cl_2"
+    Clas3_ID = "cl_3"
+
+    Wav_Files = []
+    for d, dirs, files in os.walk(SourceDirectory):
+        for file in files:
+            if file.endswith(".wav"):
+                Wav_Files.append(file)
+    for fwav in Wav_Files:
+        if Clas1_ID in fwav:
+            Clas1_Files.append(fwav)
+        if Clas2_ID in fwav:
+            Clas2_Files.append(fwav)
+        if Clas3_ID in fwav:
+            Clas3_Files.append(fwav)
+
+    TestSize = len(Clas1_Files)
+    if TestSize > len(Clas2_Files):
+        TestSize = len(Clas2_Files)
+    if TestSize > len(Clas3_Files):
+        TestSize = len(Clas3_Files)
+    ValidSize = int(TestSize * ValidPercent)
+    TestSize = int(TestSize * TestPercent)
+
+    # ------------------TEST----------------------------------------
+
+    for i in range(TestSize):
+        Index = random.randint(0, len(Clas1_Files) - 1)
+        Test_Files.append(Clas1_Files[Index])
+        del Clas1_Files[Index]
+        Index = random.randint(0, len(Clas2_Files) - 1)
+        Test_Files.append(Clas2_Files[Index])
+        del Clas2_Files[Index]
+        Index = random.randint(0, len(Clas3_Files) - 1)
+        Test_Files.append(Clas3_Files[Index])
+        del Clas3_Files[Index]
+
+    for tFile in Test_Files:
+        src = SourceDirectory + "\\"+ tFile
+        dst = TestDirectory + "\\"+ tFile
+        copyfile(src, dst)
+   # ------------------TEST---------------------------------------
+
+
+  # ------------------Valid----------------------------------------
+    Test_Files = []
+    for i in range(ValidSize):
+        Index = random.randint(0, len(Clas1_Files) - 1)
+        Test_Files.append(Clas1_Files[Index])
+        del Clas1_Files[Index]
+        Index = random.randint(0, len(Clas2_Files) - 1)
+        Test_Files.append(Clas2_Files[Index])
+        del Clas2_Files[Index]
+        Index = random.randint(0, len(Clas3_Files) - 1)
+        Test_Files.append(Clas3_Files[Index])
+        del Clas3_Files[Index]
+
+    for tFile in Test_Files:
+        src = SourceDirectory + "\\"+ tFile
+        dst = ValidDirectory + "\\"+ tFile
+        copyfile(src, dst)
+    # ------------------Valid---------------------------------------
+
+
+    Train_Files = Clas1_Files + Clas2_Files + Clas3_Files
+    for tFile in Train_Files:
+        src = SourceDirectory + "\\"+  tFile
+        dst = TrainDirectory + "\\"+  tFile
+        copyfile(src, dst)
+
+ConvertTo_8K(SourceDir=r'C:\Users\valik\Downloads\Lab\data\seven', TargetDirectory=r'C:\Users\valik\Downloads\Lab\res_data', Prefics='seven', ClassType='cl_1')
+ConvertTo_8K(SourceDir=r'C:\Users\valik\Downloads\Lab\data\cat', TargetDirectory=r'C:\Users\valik\Downloads\Lab\res_data', Prefics='cat', ClassType='cl_2')
+ConvertTo_8K(SourceDir=r'C:\Users\valik\Downloads\Lab\data\tree', TargetDirectory=r'C:\Users\valik\Downloads\Lab\res_data', Prefics='other', ClassType='cl_3')
+ConvertTo_8K(SourceDir=r'C:\Users\valik\Downloads\Lab\data\six', TargetDirectory=r'C:\Users\valik\Downloads\Lab\res_data', Prefics='other', ClassType='cl_3')
+ConvertTo_8K(SourceDir=r'C:\Users\valik\Downloads\Lab\data\house', TargetDirectory=r'C:\Users\valik\Downloads\Lab\res_data', Prefics='other', ClassType='cl_3')
+
+Divide_TrainTestValid(SourceDirectory=r'C:\Users\valik\Downloads\Lab\res_data',
+                          TrainDirectory=r'C:\Users\valik\Downloads\Lab\Train',
+                          TestDirectory=r'C:\Users\valik\Downloads\Lab\Test',
+                          ValidDirectory=r'C:\Users\valik\Downloads\Lab\Valid',
+                          TestPercent=0.2,
+                          ValidPercent=0.2)
